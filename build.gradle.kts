c2924605c6d528e1b06c206969ee3beb21c1765f
Artyom Nesin <artem.nesin@gmail.com>
2026-09-04 17:47:11 +0400

    build: upgrade toolchain to gradle 9, kotlin 2.4, ktor 3.5
    Targets JDK 21 via jvmToolchain. Dependency versions moved to a version
    catalog since 'by project' delegates are deprecated in Gradle 9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val jackson_version: String = "2.15.2"
-
 plugins {
     application
-    kotlin("jvm") version "1.7.22"
-    id("io.ktor.plugin") version "2.2.1"
-    kotlin("plugin.serialization") version "1.7.20"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
 }
 
 group = "ru.gbzlat"
 version = "0.0.1"
+
+kotlin {
+    jvmToolchain(21)
+}
+
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 
@@ -26,34 +25,32 @@ repositories {
 
 dependencies {
     // core
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-default-headers:$ktor_version")
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.default.headers)
+    implementation(libs.logback.classic)
 
     // auth
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-server-cors:$ktor_version")
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.cors)
 
     // json
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.jackson.datatype.jsr310)
 
     // database
-    implementation("org.ktorm:ktorm-core:3.6.0")
-    implementation("org.ktorm:ktorm-jackson:3.6.0")
-    implementation("org.ktorm:ktorm-support-mysql:3.6.0")
-    implementation("mysql:mysql-connector-java:8.0.32")
+    implementation(libs.ktorm.core)
+    implementation(libs.ktorm.jackson)
+    implementation(libs.ktorm.support.mysql)
+    runtimeOnly(libs.mysql.connector)
 
     // telegram
-    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
+    implementation(libs.telegram.bot)
 
     // test
-    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test)
 }
