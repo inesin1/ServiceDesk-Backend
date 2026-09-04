37cb0471bcf7bbcd9280512aa73affee269d2cda
Artyom Nesin <artem.nesin@gmail.com>
2026-09-04 17:47:11 +0400

    refactor: drop unused kotlinx.serialization annotations
    Content negotiation runs on Jackson, so @Serializable was never used.
    LocalDateTimeSerializer.serialize() was an empty body.

--- src/main/kotlin/ru/gbzlat/dto/SimpleData.kt
+++ src/main/kotlin/ru/gbzlat/dto/SimpleData.kt
@@ -1,9 +1,7 @@
 package ru.gbzlat.dto
 
-import kotlinx.serialization.Serializable
 
 // Простые данные (name)
-@Serializable
 data class SimpleData(
     val name: String
 )
--- src/main/kotlin/ru/gbzlat/plugins/Serialization.kt
+++ src/main/kotlin/ru/gbzlat/plugins/Serialization.kt
@@ -2,18 +2,13 @@ package ru.gbzlat.plugins
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import io.ktor.serialization.jackson.*
 
 val objectMapper = ObjectMapper()
+
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         jackson()
@@ -22,15 +17,4 @@ fun Application.configureSerialization() {
     objectMapper.registerModule(JavaTimeModule())
     objectMapper.findAndRegisterModules()
     objectMapper.setDateFormat(SimpleDateFormat("yyyy-MM-dd HH:mm a z"))
-}
-
-object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-    override fun deserialize(decoder: Decoder): LocalDateTime =
-        LocalDateTime.parse(decoder.decodeString())
-
-    override fun serialize(encoder: kotlinx.serialization.encoding.Encoder, value: LocalDateTime) {
-
-    }
-
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
 }
