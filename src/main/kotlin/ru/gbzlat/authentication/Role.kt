d9180803876e4fc5358534da142882d6960411ac
Artyom Nesin <artem.nesin@gmail.com>
2026-09-07 12:48:03 +0400

    feat: add role based route authorization

---
+++ src/main/kotlin/ru/gbzlat/authentication/Role.kt
@@ -0,0 +1,11 @@
+package ru.gbzlat.authentication
+
+enum class Role(val id: Int) {
+    EMPLOYEE(1),
+    SPECIALIST(2),
+    ADMIN(3);
+
+    companion object {
+        fun byId(id: Int): Role? = entries.find { it.id == id }
+    }
+}
