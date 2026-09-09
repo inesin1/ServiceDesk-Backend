f2ac53008bbf4faf3b2f829fdd6edb8b54bbd688
Artyom Nesin <artem.nesin@gmail.com>
2026-09-09 14:07:07 +0400

    refactor: expose timestamps as iso strings in response dtos

--- src/main/kotlin/ru/gbzlat/error/ApiException.kt
+++ src/main/kotlin/ru/gbzlat/error/ApiException.kt
@@ -4,8 +4,6 @@ import io.ktor.http.*
 
 class ApiException(val status: HttpStatusCode, override val message: String) : RuntimeException(message)
 
-data class ErrorResponse(val status: Int, val message: String)
-
 fun badRequest(message: String): Nothing = throw ApiException(HttpStatusCode.BadRequest, message)
 
 fun notFound(message: String): Nothing = throw ApiException(HttpStatusCode.NotFound, message)
