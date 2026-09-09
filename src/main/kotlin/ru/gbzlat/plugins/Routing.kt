8e68921e92c1cc7db9b5a0848b8bfe4507911967
Artyom Nesin <artem.nesin@gmail.com>
2026-09-09 14:07:07 +0400

    feat: serve openapi spec at /api.json and swagger ui at /swagger

--- src/main/kotlin/ru/gbzlat/plugins/Routing.kt
+++ src/main/kotlin/ru/gbzlat/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package ru.gbzlat.plugins
 
+import io.github.smiley4.ktoropenapi.openApi
+import io.github.smiley4.ktorswaggerui.swaggerUI
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.routing.*
@@ -7,9 +9,12 @@ import ru.gbzlat.routes.*
 
 fun Application.configureRouting() {
     routing {
+        route("api.json") { openApi() }
+        route("swagger") { swaggerUI("/api.json") }
+
         route("/api") {
             authRoute()
-            authenticate ("auth-jwt") {
+            authenticate("auth-jwt") {
                 userRoute()
                 ticketRoute()
                 departmentRoute()
