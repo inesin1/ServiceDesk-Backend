cd51607613ea60eb6252a23678c5ad3e2067304f
Artyom Nesin <artem.nesin@gmail.com>
2026-09-10 12:23:50 +0400

    fix: close the connection pool when the application stops

--- src/main/kotlin/ru/gbzlat/db/DatabaseFactory.kt
+++ src/main/kotlin/ru/gbzlat/db/DatabaseFactory.kt
@@ -2,17 +2,19 @@ package ru.gbzlat.db
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.v1.jdbc.Database
 import ru.gbzlat.config.AppConfig
 
-fun connectDatabase(config: AppConfig.Database): Database {
+fun Application.connectDatabase(config: AppConfig.Database): Database {
     val dataSource = HikariDataSource(HikariConfig().apply {
         jdbcUrl = "jdbc:postgresql://${config.hostname}:${config.port}/${config.name}"
         username = config.username
         password = config.password
         maximumPoolSize = config.poolSize
     })
+    monitor.subscribe(ApplicationStopped) { dataSource.close() }
 
     Flyway.configure().dataSource(dataSource).load().migrate()
 
